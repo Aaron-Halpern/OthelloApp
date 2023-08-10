d3d98c185ea296491de27b29f9cd95075f88ffed
dassoulin <dassouli@student.touro.edu>
2023-08-10 16:30:28 -0400

    Added app logo and Othello Rules menu option B"H

--- app/src/main/java/com/example/othello/activities/MainActivity.java
+++ app/src/main/java/com/example/othello/activities/MainActivity.java
@@ -1,14 +1,15 @@
-package com.example.othello;
+package com.example.othello.activities;
+
+import static com.example.othello.lib.Utils.showInfoDialog;
 
 import android.os.Bundle;
 
+import com.example.othello.R;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
-
-import androidx.core.view.WindowCompat;
 
 import com.example.othello.databinding.ActivityMainBinding;
 
@@ -18,6 +19,7 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private Snackbar mSnackBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,25 +42,39 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        int itemId = item.getItemId();
+        if (itemId == R.id.action_settings) {
+            //showSettings();
+            return true;
+        }  else if (itemId == R.id.action_rules) {
+            showRules();
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
+
+    private void showRules() {
+        showInfoDialog(MainActivity.this, "Othello Rules",
+                "Welcome to Othello!\n\n" +
+                        "Othello is a two player game.\n" +
+                        "You will be playing against the computer.\n" +
+                        "Begin by selecting your color of choice.\n" +
+                        "Black goes first.\n" +
+                        "When it is your turn, select a square to place your piece. " +
+                        "You want to sandwich your opponent's piece(s) horizontally, " +
+                        "vertically, or diagonally.\n" +
+                        "You keep on playing until one of the players runs out of moves on " +
+                        "their turn.\n" +
+                        "The player with the most amount of pieces of their color by the game's " +
+                        "end is the winner!");
+    }
+
 
 
 }
---
+++ app/src/main/java/com/example/othello/lib/Utils.java
@@ -0,0 +1,39 @@
+package com.example.othello.lib;
+
+import android.content.Context;
+import android.content.DialogInterface;
+
+import androidx.appcompat.app.AlertDialog;
+
+import com.example.othello.R;
+
+
+public class Utils {
+
+    public static void showInfoDialog (Context context, String strTitle, String strMsg)
+    {
+        // create the listener for the dialog
+        final DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener ()
+        {
+            @Override
+            public void onClick (DialogInterface dialog, int which)
+            {
+                dialog.dismiss();
+            }
+        };
+
+        // Create the AlertDialog.Builder object
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder (context);
+
+        // Use the AlertDialog's Builder Class methods to set the title, icon, message, et al.
+        // These could all be chained as one long statement, if desired
+        alertDialogBuilder.setTitle (strTitle);
+        alertDialogBuilder.setIcon (R.mipmap.ic_launcher);
+        alertDialogBuilder.setMessage (strMsg);
+        alertDialogBuilder.setCancelable (true);
+        alertDialogBuilder.setNeutralButton (context.getString (android.R.string.ok), listener);
+
+        // Create and Show the Dialog
+        alertDialogBuilder.show ();
+    }
+}
